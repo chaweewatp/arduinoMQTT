fc2c71a1c929dc80f363544843171b9f9d8af074
pornchai chaweewat <backendteam.pea@gmail.com>
2021-07-07 14:29:41 +0700

    edit

--- myIot/__init__.py
+++ myIot/__init__.py
@@ -1,6 +1,20 @@
 import paho.mqtt.client as mqtt
 
+import pyrebase
 
+config = {
+    "apiKey": "AIzaSyAZFtoOAhpIu1Y96McMpQkxY0eaAhcLWWo",
+    "authDomain": "arduino-mqtt99.firebaseapp.com",
+    "databaseURL": "https://arduino-mqtt99-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "storageBucket": "arduino-mqtt99.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(config)
+
+
+def updateData(data):
+    db = firebase.database()
+    db.child('report').push({'text':data})
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -14,6 +28,6 @@ def on_message(client, userdata, msg):
 
     if msg.topic=='report':
         print(data)
-
+        updateData(data)
 
 client = mqtt.Client()
--- myIot/views.py
+++ myIot/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .__init__ import updateData
+
+
+import pyrebase
+
 
 # Create your views here.
+def testUpdateData(request,data):
+
+    print(data)
+    updateData(data)
+    # config = {
+    #     "apiKey": "AIzaSyAZFtoOAhpIu1Y96McMpQkxY0eaAhcLWWo",
+    #     "authDomain": "arduino-mqtt99.firebaseapp.com",
+    #     "databaseURL": "https://arduino-mqtt99-default-rtdb.asia-southeast1.firebasedatabase.app",
+    #     "storageBucket": "arduino-mqtt99.appspot.com",
+    # }
+    #
+    # firebase = pyrebase.initialize_app(config)
+    # db = firebase.database()
+    # db.child('report').push({'text': data})
+
+    return  HttpResponse('Done: {}'.format(data))
